12fb7fd6b6b1178e0d43945a9b9973ae7f90b561
adenkesuma <adenfdfd10@gmail.com>
2023-06-01 14:39:24 +0700

    configure lspkind.rc.lua and cmp.rc.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,7 +12,13 @@ packer.startup(function(use)
 		'svrana/neosolarized.nvim',
 		requires = { 'tjdevries/colorbuddy.nvim' }
 	}
+  use 'kyazdani42/nvim-web-devicons' -- File icons
+  use 'L3MON4D3/LuaSnip' -- Snippet
   use 'hoob3rt/lualine.nvim' -- Statusline
+  use 'onsails/lspkind-nvim' -- vscode like pictograms
+  use 'hrsh7th/cmp-buffer' -- nvim cmp source for buffer words
+  use 'hrsh7th/cmp-nvim-lsp' -- nvim cmp source for neovim's built-in LSP
+  use 'hrsh7th/nvim-cmp' -- completion
   use 'neovim/nvim-lspconfig' -- LSP
 end)
 
--- plugin/lspconfig.rc.lua
+++ plugin/lspconfig.rc.lua
@@ -29,7 +29,8 @@ nvim_lsp.sumneko_lua.setup {
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true)
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false
       }
     }
   }
