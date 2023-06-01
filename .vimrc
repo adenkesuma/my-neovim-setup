65d86032fb7c7f76b007834661c43883ebddb7bd
adenkesuma <adenfdfd10@gmail.com>
2023-06-01 13:25:29 +0700

    first commit

---
+++ .vimrc
@@ -0,0 +1,7 @@
+call plug#begin('~/.vim/plugged')
+
+Plug 'tjdevries/colorbuddy.vim'
+Plug 'tjdevries/gruvbuddy.nvim'
+
+call plug#end()
+
---
+++ lua/plugins.lua
@@ -0,0 +1,19 @@
+local status, packer = pcall(require, 'packer')
+if (not status) then
+	print('Packer is not installed')
+	return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+	use 'wbthomason/packer.nvim'
+	use {
+		'svrana/neosolarized.nvim',
+		requires = { 'tjdevries/colorbuddy.nvim' }
+	}
+  use 'hoob3rt/lualine.nvim' -- Statusline
+  use 'neovim/nvim-lspconfig' -- LSP
+end)
+
+
